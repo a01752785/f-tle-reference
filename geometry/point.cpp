e580d0b0293d35910f187ca0cda67a8f9ec1352a
a01752785 <a01752785@tec.mx>
2023-02-20 12:30:06 -0600

    Adding geometry library

---
+++ geometry/point.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ftype;
+struct point2d {
+    ftype x, y;
+    point2d() {}
+    point2d(ftype x, ftype y): x(x), y(y) {}
+    point2d& operator+=(const point2d& t) {
+        x += t.x;
+        y += t.y;
+        return *this;
+    }
+    point2d& operator-=(const point2d& t) {
+        x -= t.x;
+        y -= t.y;
+        return *this;
+    }
+    point2d& operator*=(ftype t) {
+        x *= t;
+        y *= t;
+        return *this;
+    }
+    point2d& operator/=(ftype t) {
+        x /= t;
+        y /= t;
+        return *this;
+    }
+    point2d operator+(const point2d& t) {
+        return point2d(*this) += t;
+    }
+    point2d operator-(const point2d& t) {
+        return point2d(*this) -= t;
+    }
+    point2d operator*(ftype t) const {
+        return point2d(*this) *= t;
+    }
+    point2d operator/(ftype t) const {
+        return point2d(*this) /= t;
+    }
+};
+point2d operator*(ftype a, point2d b) {
+    return b * a;
+}
+
+struct point3d {
+    ftype x, y, z;
+    point3d() {}
+    point3d(ftype x, ftype y, ftype z): x(x), y(y), z(z) {}
+    point3d& operator+=(const point3d& t) {
+        x += t.x;
+        y += t.y;
+        z += t.z;
+        return *this;
+    }
+    point3d& operator-=(const point3d& t) {
+        x -= t.x;
+        y -= t.y;
+        z -= t.z;
+        return *this;
+    }
+    point3d& operator*=(ftype t) {
+        x *= t;
+        y *= t;
+        z *= t;
+        return *this;
+    }
+    point3d& operator/=(ftype t) {
+        x /= t;
+        y /= t;
+        z /= t;
+        return *this;
+    }
+    point3d operator+(const point3d& t) {
+        return point3d(*this) += t;
+    }
+    point3d operator-(const point3d& t) {
+        return point3d(*this) -= t;
+    }
+    point3d operator*(ftype t) const {
+        return point3d(*this) *= t;
+    }
+    point3d operator/(ftype t) const {
+        return point3d(*this) /= t;
+    }
+};
+point3d operator*(ftype a, point3d b) {
+    return b * a;
+}
+
+ftype norm(point2d a) {
+    return dot(a, a);
+}
+double abs(point2d a) {
+    return sqrt(norm(a));
+}
+double proj(point2d a, point2d b) {
+    return dot(a, b) / abs(b);
+}
+double angle(point2d a, point2d b) {
+    return acos(dot(a, b) / abs(a) / abs(b));
+}
+point3d cross(point3d a, point3d b) {
+    return point3d(a.y * b.z - a.z * b.y,
+                   a.x * b.z - a.z * b.x,
+                   a.x * b.y - a.y * b.x);
+}
+ftype triple(point3d a, point3d b, point3d c) {
+    return dot(a, cross(a, b));
+}
+ftype cross(point2d a, point2d b) {
+    return a.x * b.y - a.y * b.x;
+}
+point2d intersect(point2d a1, point2d d1, point2d a2, point2d d2) {
+    return a1 + cross(a2 - a1, d2) / cross(d1 * d2) * d1;
+}
